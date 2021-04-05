fd0727db13f803bbc182490ac61127bf1b922673
mabutalid <theoaguilar123@gmail.com>
2021-04-05 15:18:20 +0800

    finalize v2 navbar design

--- src/layout/navbar.tsx
+++ src/layout/navbar.tsx
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { 
+import {
     Flex,
     ListItem,
     UnorderedList,
     Divider,
     Text,
     ScaleFade,
+    Slide,
+    useDisclosure,
  } from "@chakra-ui/react"
  
  import { CloseIcon } from '@chakra-ui/icons'
@@ -18,71 +20,107 @@ export interface NavbarProps {
 }
  
 const Navbar: React.FC<NavbarProps> = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const { isOpen, onToggle } = useDisclosure()
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-    const handleShowNav = () => {
-        setIsOpen(!isOpen);
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+       window.addEventListener('resize', handleResize)
+       return () => {
+           window.removeEventListener('resize', handleResize)
+       }
+    }, [])
+
+    const TransitionWrapper = (props: any) => {
+        if (windowWidth < 480) {
+            return(
+                <Slide direction="left" in={isOpen} style={{ zIndex: 10 }}>
+                    {props.children}
+                </Slide>
+            );
+            
+        } 
+        
+        return (
+            <ScaleFade initialScale={0.9} in={isOpen}>
+                {props.children}
+            </ScaleFade>
+        )
     }
 
     if (isOpen) {
         return ( 
-            <ScaleFade initialScale={0.9} in={isOpen}>
-                <Flex w='160px' h='100vh'>
-                    <UnorderedList 
-                    w='100%'
-                    my='auto'
-                    py='6'
-                    bg='blue.800'
-                    position='relative' 
-                    rounded='2xl' 
-                    color='white'
-                    fontSize='17px'
-                    listStyleType='none'
-                    textAlign='center'
-                    spacing='3'>
-                        <CloseIcon 
-                        w='10px' 
-                        position='absolute' 
-                        top='0' 
-                        right='0' 
-                        mt='10px'
-                        mr='10px'
-                        cursor='pointer' 
-                        _hover={{color: "teal.500"}}
-                        onClick={handleShowNav}
-                        />
-                        <ListItem>
-                            <Logo h='80px' mx='auto' pointerEvents="none" />
-                        </ListItem>
-                        <Divider w='80%' mx='auto' />
-                        <ListItem cursor='pointer' _hover={{color: "teal.500"}}>Main</ListItem>
-                        <ListItem cursor='pointer' _hover={{color: "teal.500"}}>Inventory</ListItem>
-                        <ListItem cursor='pointer' _hover={{color: "teal.500"}}>Stocks</ListItem>
-                    </UnorderedList>
-                </Flex>
-            </ScaleFade>
+            <TransitionWrapper>
+                <UnorderedList 
+                w={['100%', '160px']}
+                py='6'
+                bg='blue.800'
+                mx={['auto']}
+                position='fixed'
+                top={['0','200']}
+                left={['0','4']}
+                rounded={['','2xl','2xl','2xl']} 
+                color='white'
+                fontSize='17px'
+                fontFamily='Kiwi Maru'
+                listStyleType='none'
+                textAlign='center'
+                spacing='3'>
+                    <CloseIcon 
+                    w='10px' 
+                    position='absolute' 
+                    top='0' 
+                    right='0'
+                    mt='10px'
+                    mr='10px'
+                    cursor='pointer' 
+                    _hover={{color: "teal.500"}}
+                    onClick={onToggle}
+                    />
+                    <ListItem>
+                        <Logo h='80px' mx='auto' pointerEvents="none" />
+                    </ListItem>
+                    <Divider w='80%' mx='auto' />
+                    <ListItem cursor='pointer' _hover={{color: "teal.500"}}>Main</ListItem>
+                    <ListItem cursor='pointer' _hover={{color: "teal.500"}}>Inventory</ListItem>
+                    <ListItem cursor='pointer' _hover={{color: "teal.500"}}>Stocks</ListItem>
+                </UnorderedList>
+            </TransitionWrapper>
         );
-    } 
+    }
 
     return (
-        <Flex h='100vh'>
-            <Flex w='50px' 
-                bg='gray.900' 
-                my='auto' 
-                position='relative' 
-                direction='column' 
-                roundedRight='lg' 
-                py='10px' 
-                color='white'
-                textAlign='center' 
-                cursor='pointer'
-                _hover={{color: "teal.500"}}
-                onClick={handleShowNav} >
-                    <Text>O</Text>
-                    <Text>P</Text>
-                    <Text>E</Text>
-                    <Text>N</Text>
-            </Flex>
+        <Flex 
+        direction={['row','column']}
+        bg={['teal.400','teal.400']} 
+        w={['160px','50px']}
+        h={['30px', 'auto']}
+        my={['', 'auto']} 
+        position='relative'
+        roundedRight={['','lg']}
+        rounded={['sm','']}
+        py={['6px','10px']}
+        marginTop={["2"]}
+        marginLeft={["2","0"]}
+        color='white'
+        fontSize={['10px','12px']}
+        fontFamily='Kiwi Maru'
+        textAlign='center'
+        justifyContent={['center','']}
+        cursor='pointer'
+        _hover={{color: "teal.500"}}
+        onClick={onToggle}>
+            <Text pb={['','4px']} px={['2px']}>N</Text>
+            <Text pb={['','4px']} px={['2px']}>A</Text>
+            <Text pb={['','4px']} px={['2px']}>V</Text>
+            <Text pb={['','4px']} px={['2px']}>I</Text>
+            <Text pb={['','4px']} px={['2px']}>G</Text>
+            <Text pb={['','4px']} px={['2px']}>A</Text>
+            <Text pb={['','4px']} px={['2px']}>T</Text>
+            <Text pb={['','4px']} px={['2px']}>E</Text>
         </Flex>
     )
 }
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,14 +2,22 @@ import * as React from 'react';
 
 // Layout
 import Navbar from '../layout/navbar';
-
+import { 
+    Box, Flex
+ } from "@chakra-ui/react"
 export interface RoutesProps {
     
 }
  
 const Routes: React.FC<RoutesProps> = () => {
     return ( 
-        <Navbar />
+        <>
+            <Flex h='100vh'>
+                <Navbar />
+                <Box direction='column' w='100%' h='100%' overflow='auto'>
+                </Box>
+            </Flex>
+        </>
      );
 }
  
