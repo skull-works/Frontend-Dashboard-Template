169b620991b3480f7639c7ba83f146121995b2d2
mabutalid <theoaguilar123@gmail.com>
2021-04-18 16:00:09 +0800

    applied mobile responsive to the inputs

--- src/layers/pages/sublayers/transactions/views/subview/create-item.subview.tsx
+++ src/layers/pages/sublayers/transactions/views/subview/create-item.subview.tsx
@@ -18,31 +18,39 @@ const CreateItem: React.FC<CreateItemProps> = () => {
     return ( 
         <Box
         w='100%'
+        pb='20px'
         mx='auto'
-        bg='gray.600'
         >
             <Heading fontFamily='Abel'>Create Item</Heading>
             <Stack direction='row' mt='20px' spacing='20px'>
-                <FormControl fontFamily='Abel' w='370px' h='90%'>
-                    <FormLabel fontSize='20px'>Store Name:</FormLabel>
-                    <Input w='90%' placeholder='Store Name'/>
-                    <FormLabel fontSize='20px' mt='10px'>Order Type:</FormLabel>
-                    <Input w='90%' placeholder='Order Type'/>
-                    <FormLabel fontSize='20px' mt='10px'>Store Name:</FormLabel>
-                    <Input w='90%' placeholder='Store Name'/>
-                    <FormLabel fontSize='20px' mt='10px'>Order Type:</FormLabel>
-                    <Input w='90%' placeholder='Order Type'/>
-                </FormControl>
-                <FormControl fontFamily='Abel' w='370px' h='90%'>
-                    <FormLabel fontSize='20px'>Store Name:</FormLabel>
-                    <Input w='90%' placeholder='Store Name'/>
-                    <FormLabel fontSize='20px' mt='10px'>Order Type:</FormLabel>
-                    <Input w='90%' placeholder='Order Type'/>
-                    <FormLabel fontSize='20px' mt='10px'>Store Name:</FormLabel>
-                    <Input w='90%' placeholder='Store Name'/>
-                    <FormLabel fontSize='20px' mt='10px'>Order Type:</FormLabel>
-                    <Input w='90%' placeholder='Order Type'/>
-                </FormControl>
+                <Box fontFamily='Abel' w={['50%','50%','50%','30%']} h='90%'>
+                    <FormControl fontFamily='Abel' w='100%' h='90%'>
+                        <FormLabel fontSize='20px'>Store Name:</FormLabel>
+                        <Input w='100%' placeholder='Store Name'/>
+                    </FormControl>
+                    <FormControl fontFamily='Abel' w='100%' h='90%'>
+                        <FormLabel fontSize='20px'>Order Type:</FormLabel>
+                        <Input w='100%' placeholder='Store Name'/>
+                    </FormControl>
+                    <FormControl fontFamily='Abel' w='100%' h='90%'>
+                        <FormLabel fontSize='20px'>Order Amount:</FormLabel>
+                        <Input w='100%' placeholder='Store Name'/>
+                    </FormControl>
+                </Box>
+                <Box fontFamily='Abel' w={['50%','50%','50%','30%']} h='90%'>
+                    <FormControl fontFamily='Abel' w='100%' h='90%'>
+                        <FormLabel fontSize='20px'>Store Name:</FormLabel>
+                        <Input w='100%' placeholder='Store Name'/>
+                    </FormControl>
+                    <FormControl fontFamily='Abel' w='100%' h='90%'>
+                        <FormLabel fontSize='20px'>Order Type:</FormLabel>
+                        <Input w='100%' placeholder='Store Name'/>
+                    </FormControl>
+                    <FormControl fontFamily='Abel' w='100%' h='90%'>
+                        <FormLabel fontSize='20px'>Order Amount:</FormLabel>
+                        <Input w='100%' placeholder='Store Name'/>
+                    </FormControl>
+                </Box>
             </Stack>
             <Button mt='20px' fontFamily='Abel' bg='blue.500' _hover={{ background: 'green.500' }}>Submit</Button>
         </Box>
